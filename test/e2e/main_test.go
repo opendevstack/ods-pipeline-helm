687fe459e979a57be4615776dd8e5b2d723f5983
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2023-09-29 10:57:32 +0200

    Reference preview tag of ods-pipeline

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -32,7 +32,7 @@ func testMain(m *testing.M) int {
 	}
 	nc, cleanup, err := ttr.SetupTempNamespace(
 		cc,
-		ott.InstallODSPipeline(),
+		ott.InstallODSPipeline(nil),
 		ttr.InstallTaskFromPath(
 			filepath.Join(rootPath, "build/tasks/deploy.yaml"),
 			nil,
