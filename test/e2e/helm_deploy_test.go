af6c49666c67d06ac45119d3b6f81a436d3fef41
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2023-10-09 10:37:44 +0200

    Migrate from Tekton v1beta1 to v1

--- test/e2e/helm_deploy_test.go
+++ test/e2e/helm_deploy_test.go
@@ -14,7 +14,7 @@ import (
 	ott "github.com/opendevstack/ods-pipeline/pkg/odstasktest"
 	"github.com/opendevstack/ods-pipeline/pkg/pipelinectxt"
 	ttr "github.com/opendevstack/ods-pipeline/pkg/tektontaskrun"
-	tekton "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+	tekton "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
